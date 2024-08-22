769cf1beb774864fe0e24ba37eda75714868f4fb
Eric Hansson <erichansson.united@gmail.com>
2024-08-21 20:57:54 -0400

    Using Geolocation to get the current location, and then the weaterAPI to get the weather of the current location. But sometimes there is a problem with the reading of the weather data.

--- src/components/weather_card.js
+++ src/components/weather_card.js
@@ -1,49 +1,68 @@
 import React, {useState, useEffect} from "react";
-
-const WEATHER_API_KEY = "b2944cf156c5494f9ae223216240702"; 
-const WEATHER_API_URL = "http://api.weatherapi.com/v1";
-
+import { useGeolocated } from "react-geolocated";
 
 const Weather_Card = () =>{
-    const [location, setLocation] = useState("");
-    const [weatherInfo, setWeaherInfo] = useState();
+    const [weatherData, setWeatherData] = useState([]);
+    const [celsius, setCelsius] = useState(null);
+    const [fahrenheit, setFahrenheit] = useState(null);
+    const [humidity, setHumidity] = useState(null);
+    const [region, setRegion] = useState(null);
+    const [city, setCity] = useState(null);
 
-    const changeLocation = (e) =>{
-        setLocation(e.target.value);
-    }
+    const WEATHER_API_KEY = "b2944cf156c5494f9ae223216240702"; 
+    const WEATHER_API_URL = "http://api.weatherapi.com/v1";
+
+    const {coords, isGeolocationAvailable, isGeolocationEnable} =
+        useGeolocated({
+            positionOptions: {
+                enableHighAccuracy: false,
+            },
+            userDecisionTimeout: 5000,
+        });
+
 
     async function getData() {
-        try{
-            const response = await fetch(`${WEATHER_API_URL}/current.json?key=${WEATHER_API_KEY}&q=${location}`);
-            if(!response.ok){
-                throw new Error(`Response status: ${response.status}`);            
+        if(coords){
+            const url = `${WEATHER_API_URL}/current.json?key=${WEATHER_API_KEY}&q=${coords["latitude"]},${coords["longitude"]}`;
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                  throw new Error(`Response status: ${response.status}`);
+                }
+
+                const json = await response.json();
+                setWeatherData(json);
+
+                console.log(weatherData);
+
+                setCelsius(weatherData.current["temp_c"]);
+                setFahrenheit(weatherData.current["temp_f"]);
+                setHumidity(weatherData.current["humidity"]);
+                setRegion(weatherData.location["region"]);
+                setCity(weatherData.location["name"]);
+
+                console.log(json);
+            } catch (error) {
+                console.error(error.message);
             }
-
-            setWeaherInfo(response.json());
-            console.log(weatherInfo);
-
-        } catch (error){
-            console.error(error.message);
         }
+        else{
+            console.error("Geoloaction is not available!");
+        }
+    }
 
-    };
-
-    const weatherShow = () =>{
+    useEffect(() =>{
         getData();
-    };
+    },[coords])
 
     return(
         <div id="card">
             <h1>Weather App</h1>
-            <p>Please Enter location</p>
-            <input type="text" onChange={changeLocation}/>
-            <br/>
-
-            <button onClick={weatherShow}>Submit</button>
-            <br/>
-
-            <div id="weatherInfo">
-                <p>{weatherInfo}</p>
+            <div id="weather">
+                <h4>{city},{region}</h4>
+                <p>Celsius: {celsius} °C</p>
+                <p>Fahrenheit: {fahrenheit} °F</p>
+                <p>Humidity: {humidity} %</p>
             </div>
         </div>
     );
